5accb1730a91b9de058ae2e1940d7d2b265fa013
raphaelbaere <raphaelbaere@id.uff.br>
2023-03-14 16:23:00 -0300

    requisito 1 concluído

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const express = require('express');
 
+const fs = require('fs').promises;
+
+const readJsonData = async (path) => {
+    try {
+        const data = await fs.readFile(path);
+        const parsedData = JSON.parse(data);
+        return parsedData;
+    } catch (error) {
+        console.error(`Erro na escrita do arquivo: ${error}`);
+    }
+};
+
+const writeJsonData = async (path, json) => {
+    try {
+      const JSONStringify = JSON.stringify(json);
+      await fs.writeFile(path, JSONStringify);
+      console.log('Arquivo escrito com sucesso!');
+    } catch (err) {
+      console.error(`Erro ao escrever o arquivo: ${err.message}`);
+    }
+  };
+
 const app = express();
 app.use(express.json());
 
@@ -13,4 +35,14 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', async (req, res) => {
+  const allRegisteredPersons = await readJsonData('src/talker.json');
+  
+  if (allRegisteredPersons) {
+    res.status(200).json(allRegisteredPersons);
+    return;
+  }
+  res.status(200).json([]);
 });
