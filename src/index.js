479d2ff70c1d3c954163465e0526027c4cb5d150
raphaelbaere <raphaelbaere@id.uff.br>
2023-03-14 17:34:56 -0300

    requisito 5 concluído

--- src/index.js
+++ src/index.js
@@ -14,11 +14,21 @@ const readJsonData = async (path) => {
     }
 };
 
-const validateEmail = (email, regex) => regex.test(email);
+const writeJsonData = async (path, json) => {
+  try {
+    const JSONStringify = JSON.stringify(json);
+    await fs.writeFile(path, JSONStringify);
+    console.log('Arquivo escrito com sucesso!');
+  } catch (err) {
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
+  }
+};
 
-const validationMiddleware = (req, res, next) => {
+const validateWithRegex = (data, regex) => regex.test(data);
+
+const validationLoginMiddleware = (req, res, next) => {
   const { email, password } = req.body;
-  const isEmailValid = validateEmail(email, /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+  const isEmailValid = validateWithRegex(email, /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
   if (!email) {
     res.status(400).json({ message: 'O campo "email" é obrigatório' }); return;
   }
@@ -31,6 +41,75 @@ const validationMiddleware = (req, res, next) => {
   const isPasswordValid = password.length >= 6;
   if (!isPasswordValid) {
     res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); return;
+  }
+  next();
+};
+
+const validationTokenMiddleware = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    res.status(401).json({ message: 'Token não encontrado' }); return;
+  }
+  const validateToken = authorization.length === 16 && typeof authorization === 'string';
+  if (!validateToken) {
+    res.status(401).json({ message: 'Token inválido' }); return;
+  }
+  next();
+};
+
+const validationNameMiddleware = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ message: 'O campo "name" é obrigatório' }); return;
+  }
+  const isNameValid = name.length >= 3 && typeof name === 'string';
+  if (!isNameValid) {
+    res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' }); return;
+  }
+  next();
+};
+
+const validationAgeMiddleware = (req, res, next) => {
+  const { age } = req.body;
+  if (!age) {
+    res.status(400).json({ message: 'O campo "age" é obrigatório' }); return;
+  }
+  const isAgeValid = Number.isInteger(age) && age >= 18;
+  if (!isAgeValid) {
+    res.status(400).json(
+      { message: 'O campo "age" deve ser um número inteiro igual ou maior que 18' },
+      ); return;
+  }
+  next();
+};
+
+const validationTalkAndWatchedMiddleware = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk) {
+    res.status(400).json({ message: 'O campo "talk" é obrigatório' }); return;
+  }
+  const { talk: { watchedAt } } = req.body;
+  if (!watchedAt) {
+    res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' }); return;
+  }
+  const validateWatchedAt = validateWithRegex(watchedAt, /^(\d{2})\/(\d{2})\/(\d{4})$/);
+  if (!validateWatchedAt) {
+    res.status(400).json(
+      { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' },
+      ); return;
+  }
+  next();
+};
+
+const validationRateMiddleware = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  if (typeof rate === 'undefined') {
+    res.status(400).json({ message: 'O campo "rate" é obrigatório' }); return;
+  }
+  const isRateValid = Number.isInteger(rate) && rate >= 1 && rate <= 5;
+  if (!isRateValid) {
+    res.status(400).json({ message: 'O campo "rate" deve ser um número inteiro entre 1 e 5' });
+    return;
   }
   next();
 };
@@ -73,7 +152,18 @@ app.get('/talker/:id', async (req, res) => {
   res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
-app.post('/login', validationMiddleware, async (req, res) => {
+app.post('/login', validationLoginMiddleware, async (req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   res.status(200).json({ token });
+});
+
+app.post('/talker', validationTokenMiddleware,
+ validationNameMiddleware, validationAgeMiddleware,
+  validationTalkAndWatchedMiddleware, validationRateMiddleware, async (req, res) => {
+  const newPerson = req.body;
+  const registeredPersons = await readJsonData('src/talker.json');
+  newPerson.id = registeredPersons.length + 1;
+  registeredPersons.push(newPerson);
+  await writeJsonData('src/talker.json', registeredPersons);
+  res.status(201).json(newPerson);
 });
